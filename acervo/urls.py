846eafda3935c5ace437b1998536db693195f4fa
VicTML <ferreira.victorbf@gmail.com>
2026-09-16 21:28:03 -0300

    Adiciona cadastro e filtros de livros

--- acervo/urls.py
+++ acervo/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path('', views.inicio),
-]
-
-urlpatterns = [
-path(
-'livros/',
-views.lista_livros,
-name='lista'
-),
+	path('', views.inicio),
+	path('livros/', views.lista_livros, name='lista'),
+	path('livros/novo/', views.novo_livro, name='novo_livro'),
 ]
--- acervo/views.py
+++ acervo/views.py
@@ -10,21 +10,30 @@ def inicio(request):
     )
 
 from .models import Livro
+
 def lista_livros(request):
     livros = Livro.objects.all()
-    return render(
-request, 'acervo/lista.html',
-{'livros': livros}
-)
+
+    nome_query = request.GET.get('nome')
+    tipo_query = request.GET.get('tipo')
+    categoria_query = request.GET.get('categoria')
+
+    if nome_query:
+        livros = livros.filter(titulo__icontains=nome_query)
+    if tipo_query:
+        livros = livros.filter(tipo_acervo=tipo_query)
+    if categoria_query:
+        livros = livros.filter(categoria=categoria_query)
+
+    return render(request, 'acervo/lista.html', {'livros': livros})
 
 def novo_livro(request):
     if request.method == 'POST':
         form = LivroForm(request.POST)
         if form.is_valid():
-            form.save() # grava no banco
-            return redirect('lista')
-        else:
-            form = LivroForm()
+            form.save()
+            return redirect('novo_livro')
+    else:
+        form = LivroForm()
 
-        return render(request, 'acervo/form.html', {'form': form})
-
+    return render(request, 'acervo/form.html', {'form': form})
